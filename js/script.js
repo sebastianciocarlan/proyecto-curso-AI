5412d5d58a0bbb0ce283049426a5b82a6437b508
Sebastian Ciocarlan <sebastian.ciocarlan@artisoftware.com>
2024-05-31 13:29:47 +0200

    initial

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Tu código va aquí. Recordad que teneis toda la documentacion en https://p5js.org/es/reference/
+function setup() {
+    //Este codigo se ejecuta 1 vez solamente, al principio
+    createCanvas(1280, 720);
+    background(220);
+    //Esto es un circulo
+    ellipse(100, 100, 100, 100)
+}
+
+function draw() {
+    //Este codigo se ejecuta cada FRAME.
+}
